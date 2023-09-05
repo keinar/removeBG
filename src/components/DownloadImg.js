8e4a33dde22a7b92b5cbfb546c7a11b0285af74a
keinar <kelkayam@seatgeek.com>
2023-09-05 19:01:16 +0300

    part 3

--- src/components/DownloadImg.js
+++ src/components/DownloadImg.js
@@ -1,11 +1,33 @@
 import './DownloadImg.css';
+import Check from'../assets/check.png';
+import New from'../assets/new.png';
 
+function DownloadImg(props) {
 
-function DownloadImg() {
+  const downloadPro = "HD הורד";
+  
   return (
-    <div>
-        sdhdfg
+    <div className='TextDI'>
+      <section className='section1'>
+        <h2>{props.title}</h2>
+        <p>{props.subTitle}</p>
+        <button className='leftBtn'>{props.btnText}</button>
+        <p className='note'>{props.note}<img src={Check} alt='check'></img></p>
+        </section>
+
+        <hr></hr>
+
+        <section className='section2'>
+        <h2 className='pro'>Pro <img src={New} alt='new' className='newPro'></img></h2>
+        <p>1280X1920 תמונה מלאה</p>
+        <button className='leftBtn'>{downloadPro}</button>
+        <p className='note'>איכות טובה עד 25 מגה פיקסל<img src={Check} alt='check'></img></p>
+        </section>
+
     </div>
+
+   
+
   
   );
 }
--- src/components/RemoveBG.js
+++ src/components/RemoveBG.js
@@ -1,49 +1,77 @@
-
 import './RemoveBG.css';
-import close from '../assets/close.svg'
-import logo from '../assets/logo.png'
-import banner from '../assets/banner.png'
-
+import close from '../assets/close.svg';
+import logo from '../assets/logo.png';
+import banner from '../assets/banner.png';
 import DownloadImg from './DownloadImg';
+import React, { useState } from 'react';
+import Warning from '../assets/warning.svg';
 
 function RemoveBG() {
+  
+  const [activeTab, setActiveTab] = useState(false); // State to track if the button is active
+  // Function to toggle the button's active state
+  const setActive = (tabName) => {
+    setActiveTab(tabName);
+  };
+
   return (
     <div>
-        <div className='bg_div_cont'>
-            <div className='bg_div_header'>
-                <img src={close} className='close_img'/>
-                <div className="bg_div_header_title"> העלאת תמונה כדי להסיר את הרקע </div>
-                <button className="bg_div_header_button"> העלאת תמונה </button>
-                <div className="bg_div_header_subtext"> פורמטים נתמכים png, jpeg</div>
-            </div>
-
-            <div className="main_cont">
-                <div className="main_left">
-                    <div className="middle_div_left">
-                        <div className="tab_button_no_bg"> הוסר רקע </div>
-                        <div className="tab_button_original"> מקורי </div>
-                    </div>
-
-                  <div className="main_left_footer">
-                      <div className="main_left_footer_text"> על ידי העלאת תמונה אתה מסכים לתנאים וההגבלות.</div>
-                      <button className="main_left_footer_btn"> תקנון חברה </button>
-                  </div>
-
-                </div>
-
-                <div className="main_right">
-                  <div className="middle_div_right">
-                    <DownloadImg/>
-                  </div>
-                </div>
-
-            </div>
-
-            <div className="footer_cont">
-                <img src={logo} className="logo"/>
-                <a href="http://www.google.com" target="_blank"><img src={banner} className="banner"/></a>
-            </div>
+      <section className='bg_div_cont'>
+        <div className='bg_div_header'>
+          <img src={close} alt="close" className='close_img' />
+          <div className="bg_div_header_title"> העלאת תמונה כדי להסיר את הרקע </div>
+          <button className="bg_div_header_button"> העלאת תמונה </button>
+          <div className="bg_div_header_subtext"> png, jpeg :פורמטים נתמכים</div>
         </div>
+
+        <div className="main_cont">
+          <div className="main_left">
+            <div className="middle_div_left">
+                <div className='top_tool_bar'>
+                  <div
+                    className={`tab_button_no_bg ${activeTab === 'no_bg' ? 'active' : ''}`}
+                    onClick={() => setActive('no_bg')}
+                  >
+                    הוסר רקע
+                  </div>
+                  <div
+                    className={`tab_button_original ${activeTab === 'original' ? 'active' : ''}`}
+                    onClick={() => setActive('original')}
+                  >
+                    מקורי
+                  </div>
+                </div>
+              <div className='middle_main_screen'>
+                <div className='inner_top_line'>
+                <p className='warningMsg'>אל תשכח להוריד את הקבצים שלך. הם ימחקו אוטומטית כשתצא מהדף<img className="warningIcon" src={Warning} alt='warning'></img></p>
+                <button className='change_bg_color'>צבע רקע</button>
+                </div>
+                <div className='inner_div_main'></div>
+              </div>
+            </div>
+            <div className="main_left_footer">
+              <div className="main_left_footer_text"> על ידי העלאת תמונה אתה מסכים לתנאים וההגבלות.</div>
+              <button className="main_left_footer_btn"> תקנון חברה </button>
+            </div>
+          </div>
+
+          <div className="main_right">
+            <div className="middle_div_right">
+              <DownloadImg 
+                title="תמונה חינם" 
+                subTitle="612X408 תצוגה מקדימה של תמונה" 
+                btnText="הורד" 
+                note="איכות טובה עד 0.25 מגה פיקסל"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="footer_cont">
+          <img src={logo} alt="logo" className="logo" />
+          <a href="http://www.google.com" target="_blank" rel="noreferrer"> <img src={banner} alt="banner" className="banner" /></a>
+        </div>
+      </section>
     </div>
   );
 }
